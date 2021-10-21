c6a3b32ab561e3688561a4c2abbc5f3befe151a2
Collin Reich <cdreich7@gmail.com>
2021-10-20 23:50:48 -0400

    updated portfolio infomation, added dropdown

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
+let resumeClickHandler = () => {
+    window.open('/resources/resume.pdf', '_blank', 'fullscreen=yes');
+    return false;
+}
+
 //main driver function
 let main = () => {
     $(".navbar-placeholder").load("navbar.html");
     let contactForm = document.getElementById("contact-form");
     let contactFields = ["user_fname", "user_lname", "user_email", "user_phone", "user_message"];
+    document.getElementById("resume-visual").onclick = resumeClickHandler;
     // contactForm.addEventListener("submit", validateFormOnSubmit);
     const validator = new FormValidator(contactForm, contactFields);
     validator.validateOnSubmit();
